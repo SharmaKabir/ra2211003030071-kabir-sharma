d1e4726a5f9374fc9be7b5849ace3363f1217837
Kabir Sharma <sharmakabir204@gmail.com>
2025-04-25 17:23:22 +0530

    completed doctorcard comp

---
+++ src/components/DoctorCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const DoctorCard = ({ doctor}) => {
+    // if(!doctor || !doctor.name){
+    //     console.log("error", doctor);
+    //     return 
+    //     <div className="doctor-card"> 
+    //         Invalid doctor data
+    //     </div>
+    // }
+    if (!doctor || !doctor.name) {
+        console.error('Invalid', doctor);
+        return <div className="doctor-card">Invalid doctor data</div>;
+      }
+  return (
+    <div className="doctor-card" data-testid="doctor-card">
+        <div className='doctor-image'>
+            <img src={doctor.image}/>
+        </div>
+        <div className="doctor-details">
+          <h2 data-testid="doctor-name">{doctor.name}</h2>
+          <p data-testid="doctor-specialty">{doctor.specialties ? doctor.specialties.join(', ') : 'No specialties listed'}</p>
+          <p data-testid="doctor-experience">{doctor.experience || 0} years of experience</p>
+          <p data-testid="doctor-fee">Consultation Fee: ₹{doctor.fees || 0}</p>
+          <div className="doctor-consultation-type">
+            {doctor.consultationType === 'video-consult' ? 'Video Consult' : 'In Clinic'}
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default DoctorCard
